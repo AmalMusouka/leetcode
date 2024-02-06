24aa837c95b6f0f44488b01034e7bb56fb1cb4c2
Amal Aaramam Sudhikumar <amalmusouka@192.168.1.8>
2024-02-06 16:30:44 +0530

    refactor: Renamed the file and added spec

---
+++ Top_k_frequent/347_top_k_frequent.py
@@ -0,0 +1,12 @@
+# Given an integer array nums and an integer k, return the k most frequent elements.
+# You may return the answer in any order.
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        nums = [1, 2, 3, 4, 5]
